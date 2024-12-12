bc4b87dc6fcf40738fd21e45d441131c2f54cef0
aysun16 <aysunbinnetzade2@gmail.com>
2024-12-12 18:58:33 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,66 @@
+// let result;
+// let first=Number(prompt("First Number"));
+// let second=Number(prompt("Second Number"));
+// let operator= prompt("Add Operator");
+
+// // let operator=prompt("Add Operator");
+
+// function sum(x , y) {
+// return x + y ;
+// }
+// function minus(x , y) {
+//     return x - y ;
+//     }
+//     function multiplication (x , y) {
+//         return x * y ;
+//         }
+//         function devide (x , y) {
+//             return x / y ;
+//             }
+            
+//             function calc( x , y , operators) {
+//                 switch (operator){
+
+//                     case "+":
+//                         return sum(x , y);
+
+//                         case "-":
+//                         return minus(x , y);
+
+//                         case "*":
+//                         return multiplication(x , y);
+
+//                         case "/":
+//                         return devide(x , y);
+//                 }     
+//             }
+
+            
+//             console.log(calc(2, 3, "+") );
+            
+            
+
+
+let x= Number(prompt("First Number"));
+let y= Number(prompt("Second Number"));
+let operator= prompt("Operator");
+
+let result;
+
+if(operator== '+') {
+    result = x+y ;
+}
+    else if (operator== '-') {
+        result = x-y ;
+    }
+    else if (operator== '*') {
+        result = x*y ;
+}
+    else if (operator == '/') {
+        result = x / y ;
+    }
+    else {
+result=("bele cavab yoxdur");
+    }
+
+
